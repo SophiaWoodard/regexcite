d29acddb619b1d4fd79f06d5b1378497f9cb0555
Sophia Woodard <smwoodar@calpoly.edu>
2026-05-21 19:31:38 -0700

    trying again

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,4 @@
+#' Split a string
 #'
 #' @param x A character vector with one element.
 #' @param split What to split on.
@@ -11,6 +12,3 @@
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
-
-document()
-?strsplit1
