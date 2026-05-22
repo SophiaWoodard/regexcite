37c2543290a95cabfd016d9d7bdc3f3a2d083797
Sophia Woodard <smwoodar@calpoly.edu>
2026-05-21 19:20:42 -0700

    help

---
+++ R/strsplit1.R
@@ -0,0 +1,13 @@
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
+
+load_all()
+
+x <- "alfa,bravo,charlie,delta"
+strsplit1(x, split = ",")
+
+exists("strsplit1", where = globalenv(), inherits = FALSE)
+
+
+check()
