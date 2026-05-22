d5302dab42f13e71f427ce16f2f9afeb2a6e991d
Sophia Woodard <smwoodar@calpoly.edu>
2026-05-21 19:30:24 -0700

    hai

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,13 +1,16 @@
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
 
-load_all()
-
-x <- "alfa,bravo,charlie,delta"
-strsplit1(x, split = ",")
-
-exists("strsplit1", where = globalenv(), inherits = FALSE)
-
-
-check()
+document()
+?strsplit1
